50731cf9bbd85f7360e8ef7cec6ae370c44de18b
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2024-11-04 11:43:51 +0000

    Very simple version with no age structure setup

--- covid_power.R
+++ covid_power.R
@@ -1,5 +1,8 @@
 # Power analysis to estimate number of samples needed for Covid in non-detects
 set.seed(123)
+library(ggplot2)
+library(vcd)
+
 rm(list = ls())
 
 # Data from Ben 30 October 2024
@@ -8,6 +11,22 @@ obs_data <- data.frame(
   pathogen  = rep(c("no_path_det", "path_det"), each = 4),
   counts    = c(21, 31, 511, 321, 51, 51, 392, 210)
 )
+
+# Check for differences in age groups
+# Create a contingency table
+contingency_table <- xtabs(counts ~ age_group + pathogen, data = obs_data)
+# Perform the chi-squared test
+chi_squared_test <- chisq.test(contingency_table)
+print(chi_squared_test)
+# Perform pairwise comparisons
+pairwise_test <- pairwise.prop.test(contingency_table, p.adjust.method = "bonferroni")
+print(pairwise_test)
+
+# Bar plot of counts by age group and pathogen status
+ggplot(obs_data, aes(x = age_group, y = counts, fill = pathogen)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Counts by Age Group and Pathogen Status", x = "Age Group", y = "Counts") +
+  theme_classic()
 
 
 # Data from https://www.gov.uk/government/statistics/national-flu-and-covid-19-surveillance-reports-2024-to-2025-season 
@@ -29,16 +48,26 @@ samp_range <- seq(min_samp, max_samp, by = stp_samp)
 # Draws per sample (not implemented yet)
 drw_samp <- 100
 
+# Ignore age structure initially
+# Repeat the process for each sample size based on drw_samp
+p_val_tally <- data.frame(drw_samp = numeric(0), trial = numeric(0), p_val = numeric(0))
 for(trial in samp_range){
-  det_thresh <- runif(1, min = min_pop/100, max = max_pop/100)
-  det_with_covid <- ifelse(runif(trial, 0, 1) > det_thresh, 0, 1)
-  nondet_with_covid <- ifelse(runif(trial, 0, 1) > (det_thresh * non_det_xs ), 0, 1)
-  # Assemble into data.frame
-  dat_for_check <- data.frame(
-    freq = c(det_with_covid, nondet_with_covid),
-    source = c(rep("det", trial), rep("nondet", trial))
-  )
-  # Structure of data not right or maybe need a chi-sq
-  mod_res <- MASS::loglm(freq ~ source, data = dat_for_check)
-  # Count whether mod_res is significant and tally for different trial sizes
+  for(i in 1 : drw_samp){
+    det_thresh <- runif(1, min = min_pop/100, max = max_pop/100)
+    det_with_covid <- ifelse(runif(trial, 0, 1) > det_thresh, 0, 1)
+    nondet_with_covid <- ifelse(runif(trial, 0, 1) > (det_thresh * non_det_xs ), 0, 1)
+    # Assemble into data.frame
+    dat_for_check <- data.frame(
+      freq = c(det_with_covid, nondet_with_covid),
+      source = c(rep("det", trial), rep("nondet", trial))
+    )
+    mod_res <- glm(freq ~ source, data = dat_for_check)
+    p_val <- summary(mod_res)$coefficients[2, 4]
+    p_val_tally <- rbind(p_val_tally, data.frame(drw_samp = i, trial = trial, p_val = p_val))
+  }
 }
+
+ggplot(p_val_tally, aes(x = trial, y = p_val)) +
+  geom_smooth() +
+  labs(title = "Power Analysis for Covid in Non-Detects", subtitle = "Asssume 10% excess; no age structure", x = "Number of Samples", y = "P-value") +
+  theme_classic()
